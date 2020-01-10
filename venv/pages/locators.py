70ae4db25e7f184010a7bedf51f32e23027ecef3
Hoggard <hoggard22@mail.ru>
2020-01-10 15:50:21 +0300

    added Locators for login page

---
+++ venv/pages/locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_EMAIL_ADRESS = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTER_EMAIL_ADRESS = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
