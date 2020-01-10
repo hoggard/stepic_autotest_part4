67ac43bb4d598687eeaa7ee588d71113f02f4ec9
Hoggard <hoggard22@mail.ru>
2020-01-10 15:49:35 +0300

    Added tests for login page

---
+++ venv/pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/' , "login-link has wrong url"
+        
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_ADRESS), "Login Email-adress is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login Password is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_EMAIL_ADRESS), "Register Email-adress is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "Register Password is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD), "Confirm Register Password is not presented"
+        # реализуйте проверку, что есть форма регистрации на странице
