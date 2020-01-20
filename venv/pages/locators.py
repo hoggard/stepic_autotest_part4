2981918cf5c4aac5331bbd3e670e25bdedd66052
Hoggard <hoggard22@mail.ru>
2020-01-20 16:28:03 +0300

    added locators for basket tests

--- venv/pages/locators.py
+++ venv/pages/locators.py
@@ -23,3 +23,5 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, "#default header div.page_inner div div.basket-mini.pull-right.hidden-xs span a")
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
