64bccc1b0469eb99f43c1b5136b22d6d042215e6
Hoggard <hoggard22@mail.ru>
2020-01-23 15:43:42 +0300

    added basket page

---
+++ venv/pages/basket_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def guest_expect_basket_is_empty(self):
+        basket_positions = self.browser.find_elements_by_css_selector('#basket_formset div')
+        assert len(basket_positions) == 0, "basket is not empty"
+
+    def guest_should_see_empty_basket_message(self):
+        empty_basket_message = self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MESSAGE).text
+        assert "Your basket is empty." in empty_basket_message, "Empty basket message is abcent"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(
+            *BasePageLocators.USER_ICON), "User icon is not presented, probably unauthorised user"
