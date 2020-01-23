c33d10fdbe438f1577520dc43eca5f70cafcad17
Hoggard <hoggard22@mail.ru>
2020-01-23 15:47:31 +0300

    added register new user

--- venv/pages/login_page.py
+++ venv/pages/login_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-import time
 
 
 class LoginPage(BasePage):
@@ -11,7 +10,8 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # проверка на корректный url адрес
-        assert self.browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/' , "login-link has wrong url"
+        login_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/' 
+        assert self.browser.current_url == login_link , "login-link has wrong url"
         
     def should_be_login_form(self):
         # проверка, что есть форма логина
@@ -24,12 +24,11 @@ class LoginPage(BasePage):
         assert self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD), "Register Password is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD), "Confirm Register Password is not presented"
 
-    def register_new_user(email, password):
-        email = str(time.time()) + "@fakemail.org"
-        #password = "abracadabra123"
-        self.is_element_present(*LoginPageLocators.REGISTER_EMAIL_ADRESS).send_keys(email)
-        self.is_element_present(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
-        self.is_element_present(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(password)
-        self.is_element_present(*LoginPageLocators.REGISTER_BUTTON).click()
+    def register_new_user(self, email, password):
+        # регистрируем нового пользователя
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL_ADRESS).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
 
 
