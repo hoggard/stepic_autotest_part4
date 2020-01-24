66b8a5c5dc7f9316b6775ce68c76155670fef0a2
Hoggard <hoggard22@mail.ru>
2020-01-24 11:29:54 +0300

    corrected imports

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
-from pages.base_page import BasePage
-from pages.login_page import LoginPage
-from pages.product_page import ProductPage
-from pages.basket_page import BasketPage
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
